9994c49b12ded105545500acff173f0da15eaff4
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2021-10-27 00:22:46 +0700

    Version 1.1

---
+++ Assets/Scripts/OpenCard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OpenCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private Sprite heros;
+
+    public Image ObjectWithImage;
+    public Sprite spriteToChange;
+    private void Start()
+    {
+        ObjectWithImage.sprite = spriteToChange;
+    }
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+           cardBack.SetActive(false);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/Scrolling.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    [Range(1, 50)]
+    [Header("Controllers")]
+    public int panCount;
+    [Range(0, 500)]
+    public int panOffset;
+    [Range(0f, 20f)]
+    public float snapSpeed;
+    [Range(2f,20f)]
+    public float scaleSpeed;
+    [Range(0f, 5f)]
+    public float scaleOffset;
+    [Header("Other Objects")]
+    public GameObject panPrefab;
+
+    private GameObject[] instPans;
+    private Vector2[] pansPos;
+    private Vector2[] pansScale;
+
+    private RectTransform contentRect;
+    private Vector2 contentVector;
+
+    private int selectedPanID;
+    private bool isScrolling;
+    private void Start()
+    {
+        contentRect = GetComponent<RectTransform>();
+        instPans = new GameObject[panCount];
+        pansPos = new Vector2[panCount];
+        pansScale = new Vector2[panCount];
+        for (int i = 0; i < panCount; i++)
+        {
+            instPans[i] = Instantiate(panPrefab, transform, false);
+            if (i == 0) continue;
+            instPans[i].transform.localPosition = new Vector2(
+                instPans[i-1].transform.localPosition.x +
+                 panPrefab.GetComponent<RectTransform>().sizeDelta.x +
+                  panOffset,instPans[i].transform.localPosition.y);
+            pansPos[i] = -instPans[i].transform.localPosition;
+        }
+    }
+
+    private void FixedUpdate()//It is bad for optimization to create a new Vector here 
+    {
+        float nearestPos = float.MaxValue;
+        for (int i = 0; i < panCount; i++)
+        {
+            float distance = Mathf.Abs(contentRect.anchoredPosition.x - pansPos[i].x);
+            if (distance < nearestPos)
+            {
+                nearestPos = distance;
+                selectedPanID = i;
+            }
+            float scale = Mathf.Clamp(1 / (distance / panOffset) * scaleOffset, 0.5f, 1f);
+            pansScale[i].x = Mathf.SmoothStep(instPans[i].transform.localScale.x, scale, scaleSpeed * Time.fixedDeltaTime);
+            pansScale[i].y = Mathf.SmoothStep(instPans[i].transform.localScale.y, scale, scaleSpeed * Time.fixedDeltaTime);
+            instPans[i].transform.localScale = pansScale[i];
+        }
+        if (isScrolling) return;
+        contentVector.x = Mathf.SmoothStep(contentRect.anchoredPosition.x, pansPos[selectedPanID].x, 
+            snapSpeed*Time.fixedDeltaTime);
+        contentRect.anchoredPosition = contentVector;
+    }
+
+    public void myScrolling(bool scroll)
+    {
+        isScrolling = scroll;
+    }
+
+}
